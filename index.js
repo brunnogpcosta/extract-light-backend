700f2337e451a00befcbf359700a6b7bb11b31ca
Brunno Costa <b2ncostadev@gmail.com>
2023-12-22 13:21:04 -0300

    env config

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
+
 const express = require('express');
 const cors = require('cors'); 
 const pdfController = require('./src/controllers/invoicesController.js');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
@@ -11,10 +13,10 @@ app.use(express.json());
 // Dowload pdf
 app.get('/download/:fileName', pdfController.downloadInvoice);
 
-// Salvar faturas no banco de dados
+// save invoice ind DB
 app.post('/extract-invoice', pdfController.processPDF);
 
-// Obter faturas
+// get faturas
 app.get('/invoices', pdfController.getInvoices);
 
 app.listen(port, () => {
